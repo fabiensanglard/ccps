3aeb22cf8b2d0772cfec82cff50db2aa64b57f0f
Fabien Sanglard <fabien.sanglard@gmail.com>
2022-02-21 21:17:39 -0800

    Output z80 to out

--- cc/z80/make.sh
+++ cc/z80/make.sh
@@ -1,6 +1,7 @@
-sdasz80 -plogff -o crt0.rel crt0.s
-sdcc --compile-only -mz80 --data-loc 0xd000 --no-std-crt0 main.c 
+mkdir -p out
+sdasz80 -plogff -o out/crt0.rel crt0.s
+sdcc --compile-only -mz80 --data-loc 0xd000 --no-std-crt0 -o out/main.rel main.c 
 sdldz80 -nf main.lk
-objcopy --input-target=ihex --output-target=binary main.ihx main.rom
-dd if=/dev/zero of=main.rom bs=1 count=1 seek=65536
-cp main.rom ~/mame/roms/sf2/sf2_9.12a
+objcopy --input-target=ihex --output-target=binary out/main.ihx out/main.rom
+dd if=/dev/zero of=out/main.rom bs=1 count=1 seek=65536
+cp out/main.rom ~/mame/roms/sf2/sf2_9.12a
