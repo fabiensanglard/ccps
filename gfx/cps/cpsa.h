99611bbdcf8fb29b880ce8eff745b40abd12ab52
Artemio <aurbina@junkerhq.net>
2024-02-19 23:40:39 -0600

    - Filled CPSA registers - Added an extra CPSB register

--- gfx/cps/cpsa.h
+++ gfx/cps/cpsa.h
@@ -1,4 +1,19 @@
-#define CPSA_REG_SPRITES_BASE (0x0 / 2)
-// .. Complete as needed
-#define CPSA_REG_PALETTE_BASE (0xa / 2)
-// .. Complete as needed
+#define CPSA_REG_SPRITES_BASE       (0x00/2)    /* Base address of objects */
+#define CPSA_REG_SCROLL1_BASE       (0x02/2)    /* Base address of scroll 1 */
+#define CPSA_REG_SCROLL2_BASE       (0x04/2)    /* Base address of scroll 2 */
+#define CPSA_REG_SCROLL3_BASE       (0x06/2)    /* Base address of scroll 3 */
+#define CPSA_REG_OTHER_BASE         (0x08/2)    /* Base address of other video */
+#define CPSA_REG_PALETTE_BASE       (0x0a/2)    /* Base address of palette */
+#define CPSA_REG_SCROLL1_SCROLLX    (0x0c/2)    /* Scroll 1 X */
+#define CPSA_REG_SCROLL1_SCROLLY    (0x0e/2)    /* Scroll 1 Y */
+#define CPSA_REG_SCROLL2_SCROLLX    (0x10/2)    /* Scroll 2 X */
+#define CPSA_REG_SCROLL2_SCROLLY    (0x12/2)    /* Scroll 2 Y */
+#define CPSA_REG_SCROLL3_SCROLLX    (0x14/2)    /* Scroll 3 X */
+#define CPSA_REG_SCROLL3_SCROLLY    (0x16/2)    /* Scroll 3 Y */
+#define CPSA_REG_STARS1_SCROLLX     (0x18/2)    /* Stars 1 X */
+#define CPSA_REG_STARS1_SCROLLY     (0x1a/2)    /* Stars 1 Y */
+#define CPSA_REG_STARS2_SCROLLX     (0x1c/2)    /* Stars 2 X */
+#define CPSA_REG_STARS2_SCROLLY     (0x1e/2)    /* Stars 2 Y */
+#define CPSA_REG_ROWSCROLL_OFFS     (0x20/2)    /* base of row scroll offsets in other RAM */
+#define CPSA_REG_VIDEOCONTROL       (0x22/2)    /* flip screen, rowscroll enable */
+
